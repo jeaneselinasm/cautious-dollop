a2cd19eb1c9122428f99a1e637272f495c4afc43
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-08-13 22:03:16 +0900

    update api.ts for fetching data

--- sandbox/src/utils/api.ts
+++ sandbox/src/utils/api.ts
@@ -1,4 +1,4 @@
-import type { Post } from "../types/types";
+import type { Post, User } from "../types/types";
 import { API } from "./axios";
 
 export async function getPosts() : Promise<Post[]>{
@@ -7,6 +7,21 @@ export async function getPosts() : Promise<Post[]>{
 }
 
 export async function getPost(id:number) : Promise<Post> {
-    const {data} = await API.get<Post>(`/post/${id}`)
+    const {data} = await API.get<Post>(`/posts/${id}`)
+    return data
+}
+
+export async function getUsers() : Promise<User[]>{
+    const {data} = await API.get<User[]>('/users')
+    return data
+}
+
+export async function getUser(id:number) : Promise<User> {
+    const {data} = await API.get<User>(`/users/${id}`)
+    return data
+}
+
+export async function createPost(newPost : Omit<Post, "id">) : Promise<Post> {
+    const {data} = await API.post<Post>('/posts', newPost)
     return data
 }
